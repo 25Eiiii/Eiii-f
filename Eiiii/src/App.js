718538346e3747a927e89fe373de97441a10b397
yejinkyo <p2006655@gmail.com>
2025-06-23 02:01:40 +0900

    즉석 밥약 페이지 ui

--- Eiiii/src/App.js
+++ Eiiii/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import data from "./data.json"
+import Match from './pages/Match';
+
 
 function App() {
+  const [dataList, setDataList] = useState([]);
+
+  useEffect(() => {
+    setDataList(data);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Match dataList={dataList} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
