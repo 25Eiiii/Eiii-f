b7140358e556a7962c16f889c7cf272937bb3c3c
yejinkyo <p2006655@gmail.com>
2025-06-25 17:45:33 +0900

    feat: 글 상세 페이지 작업

--- Eiiii/src/pages/CmDetail.jsx
+++ Eiiii/src/pages/CmDetail.jsx
@@ -7,22 +7,21 @@ const Detail = () => {
             <D.Header>
                 <D.Back>
                     <img 
-                        id="search"
+                        id="back"
                         src={`${process.env.PUBLIC_URL}/images/back.svg`}
-                        alt="search"
+                        alt="back"
                     />
                 </D.Back>
-                <D.Search>
                     <img 
                         id="search"
                         src={`${process.env.PUBLIC_URL}/images/search.svg`}
                         alt="search"
                     />
-                </D.Search>
                 <D.Title>선후배 밥약
                     <img 
                         id="meal"
                         src={`${process.env.PUBLIC_URL}/images/meal.svg`}
+                        alt="meal"
                     />
                 </D.Title>
             </D.Header>
@@ -31,6 +30,7 @@ const Detail = () => {
                     <D.Pic>
                         <img 
                         src={`${process.env.PUBLIC_URL}/images/avatar.svg`}
+                        alt="img"
                         />
                     </D.Pic>
                     <D.Infos>
@@ -44,18 +44,19 @@ const Detail = () => {
                     </D.Infos>
                     <D.Apply>대화 신청</D.Apply>
                 </D.Profile>
+                <D.Big>새로 생긴 훠궈집 같이 가실 분 4분 구해요!</D.Big>
                 <D.Content>
-                    <D.Big>새로 생긴 훠궈집 같이 가실 분 4분 구해요!</D.Big>
                     <p>4명이서 같이 가면 개업 이벤트로 20퍼 할인 해준대요!! 그 달에 생일인 사람있는 테이블은 추가로 10퍼 할인 더 해준다는데 같이 가요~~~!!! 저 5월 생일이에요!!!! 후배 선배 상관 없이 다 좋습니다 ㅎㅎ 엄청 맛있어보이죠~~!!!</p>
                     <D.Food>
                     <img 
                         src={`${process.env.PUBLIC_URL}/images/food.svg`}
+                        alt="food"
                     />
                     </D.Food>   
                 </D.Content>
                 <D.Bottom>
                 <D.Like>
-                    <D.IconImg src={`${process.env.PUBLIC_URL}/images/heart.svg`} />
+                    <D.IconImg src={`${process.env.PUBLIC_URL}/images/heart.svg`} alt="heart"/>
                     좋아요 5개
                 </D.Like>
                 <D.Comment>
@@ -70,14 +71,89 @@ const Detail = () => {
             </D.Detail>
             <D.Line></D.Line>
             <D.Comments>
-                <D.C1>
-
-                </D.C1>
-                <D.Line2></D.Line2>
+                <D.Profile>
+                    <D.Pic>
+                        <img 
+                        src={`${process.env.PUBLIC_URL}/images/avatar.svg`}
+                        alt="img"
+                        />
+                    </D.Pic>
+                    <D.Infos>
+                        <D.Info1 variant="cmt">
+                            <D.Name>김치삼겹살</D.Name>
+                            <D.Time>5분 전</D.Time>
+                        </D.Info1>
+                        <D.Info2>
+                            성악과26
+                        </D.Info2>
+                    </D.Infos>
+                    <D.Apply>대화 신청</D.Apply>
+                </D.Profile>
+                <D.Content variant="cmt">
+                    <p>
+                    헐 저요!!! 저 가보고 싶었는데 ㅎㅎㅎㅎㅎㅎㅎ <br />
+                    거기 후기가 엄청 좋던데요??? <br />
+                    새내기인데 괜찮으시면 같이 가요 선배님
+                    </p>
+                    <D.CBtm>
+                    <D.Good>
+                        <img
+                            src={`${process.env.PUBLIC_URL}/images/good.svg`}
+                            alt="good"
+                        />
+                        좋아요 1개
+                    </D.Good>
+                    <D.Comment>
+                        <img
+                            src={`${process.env.PUBLIC_URL}/images/comment.svg`}
+                            alt="comment"
+                        />
+                        답글 달기
+                    </D.Comment>
+                </D.CBtm>
+                </D.Content>
                 <D.C2>
-
+                <D.Profile>
+                    <D.Pic>
+                        <img 
+                        src={`${process.env.PUBLIC_URL}/images/avatar.svg`}
+                        alt="img"
+                        />
+                    </D.Pic>
+                    <D.Infos>
+                        <D.Info1 variant="cmt">
+                            <D.Name>오리고기 먹고싶다</D.Name>
+                            <D.Time>방금</D.Time>
+                        </D.Info1>
+                        <D.Info2>
+                            정보통계학과21
+                        </D.Info2>
+                    </D.Infos>
+                    <D.Apply>대화 신청</D.Apply>
+                </D.Profile>
+                <D.Content variant="cmt">
+                    <p>오 대박! 너무 좋죠 !!!!!!!! 대화 신청 걸게요!!!</p>
+                <D.CBtm>
+                    <D.Good>
+                        <img
+                            src={`${process.env.PUBLIC_URL}/images/good.svg`}
+                            alt="good"
+                        />
+                        좋아요
+                    </D.Good>
+                </D.CBtm>
+                </D.Content>
                 </D.C2>
             </D.Comments>
+            <D.Line2></D.Line2>
+            <D.Write>
+                <img 
+                    src={`${process.env.PUBLIC_URL}/images/cam.svg`}
+                    alt="cam"
+                />
+                <D.Here placeholder="댓글을 입력해 주세요."></D.Here>
+                <D.Btn>등록</D.Btn>
+            </D.Write>
         </D.Container>
 
     );
--- Eiiii/src/styles/pages/styledCmDetail.jsx
+++ Eiiii/src/styles/pages/styledCmDetail.jsx
@@ -15,7 +15,6 @@ export const Container = styled.div`
     border: 1px solid #6A5656;
     background: #FFF;
     box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    overflow: hidden;
 `
 
 export const Header = styled.div`
@@ -36,10 +35,6 @@ export const Back = styled.div`
     align-items: center;
     justify-content: center;
     border-radius: 5px;
-`
-
-export const Search = styled.div`
-
 `
 
 export const Title = styled.div`
@@ -85,7 +80,6 @@ export const Infos = styled.div`
 export const Info1 = styled.div`
     display: flex;
     align-items: center;
-    gap: 8px;
 `
 
 export const Name = styled.div`
@@ -123,32 +117,33 @@ export const Info2 = styled.div`
 `
 
 export const Apply = styled.div`
-display: inline-flex;
-padding: 6px 12px;
-align-items: center;
-border-radius: 999px;
-background: #F99505;
-color: var(--Font-White, #FFFFFF);
-text-align: center;
-font-family: "Spoqa Han Sans Neo";
-font-size: 12px;
-font-style: normal;
-font-weight: 500;
-line-height: 100%; /* 12px */
-letter-spacing: -0.12px;
-position: absolute;
-right: 27px;
+    display: inline-flex;
+    padding: 6px 12px;
+    align-items: center;
+    border-radius: 999px;
+    background: #F99505;
+    color: var(--Font-White, #FFFFFF);
+    text-align: center;
+    font-family: "Spoqa Han Sans Neo";
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 100%; /* 12px */
+    letter-spacing: -0.12px;
+    position: absolute;
+    right: 27px;
 `
 
 export const Content = styled.div`
-color: #000;
-font-family: Inter;
-font-size: 12px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-margin-top: 14px;
-margin-bottom: 40px;
+    color: #000;
+    font-family: Inter;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-bottom: 40px;
+    margin-left: ${(props) => (props.variant === "cmt" ? "41px" : "0px")};
+    margin-top: ${(props) => (props.variant === "cmt" ? "-8px" : "0px")};
 `
 
 export const Big = styled.div`
@@ -159,12 +154,12 @@ export const Big = styled.div`
     font-weight: 700;
     line-height: 100%; /* 16px */
     letter-spacing: -0.16px;
+    margin-top: 14px;
 `
 export const Food = styled.div`
-width: 375px;
-height: 208px;
-flex-shrink: 0;
-aspect-ratio: 375/208;
+    width: 375px;
+    height: 208px;
+    flex-shrink: 0;
 `
 
 export const Bottom = styled.div`
@@ -181,7 +176,7 @@ export const Bottom = styled.div`
 `
 
 export const IconImg = styled.img`
-margin-right: 3px;
+    margin-right: 3px;      
 `
 export const Like =  styled.div`
     display: flex;
@@ -190,6 +185,7 @@ export const Like =  styled.div`
 export const Comment =  styled.div`
     display: flex;
     align-items: center;
+    gap: 2px;
 `
 export const Share =  styled.div`
     display: flex;
@@ -197,24 +193,84 @@ export const Share =  styled.div`
 `
 
 export const Line = styled.div`
-width: 437px;
-height: 3px;
-flex-shrink: 0;
-background: #D9D9D9;
-margin-top: 10px;
+    width: 437px;
+    height: 3px;
+    flex-shrink: 0;
+    background: #D9D9D9;
+    margin-top: 10px;
 `
 
 export const Comments = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    align-self: flex-start;
+    margin-left: 13px;
+    margin-top: 11px;
 `
 
-export const C1 = styled.div`
-
+export const CBtm = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 25px;
+    color: #F8B621;
+    font-family: Inter;
+    font-size: 8px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
 `
+
+export const Good = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 2px;
+`
+
 export const Line2 = styled.div`
-
+    width: 440px;
+    height: 1px;
+    flex-shrink: 0;
+    background: #F8B621;
+    position: absolute;
+    bottom: 69px;
+`
+export const C2 = styled.div`
+    margin-left: 48px;
 `
 
-export const C2 = styled.div`
+export const Write = styled.div`
+    display: flex;
+    margin-left: 30px;
+    align-items: center;
+    position: absolute;
+    bottom: 20px;
+    left: -5px;
+    right: 30px;
+    color: #F8B621;
+    justify-content: space-between;
+    gap: 10px;
+`
 
+export const Here = styled.input`
+    flex: 1;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    border: none;
+    outline: none;
+    &::placeholder {
+        color: #F8B621;
+    }
+`
+
+export const Btn = styled.div`
+    color: #F8B621;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    cursor: pointer;
 `
