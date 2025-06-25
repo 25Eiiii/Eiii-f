3db55d71cfb760bfebbfb05b86eef3f16de1f168
yejinkyo <p2006655@gmail.com>
2025-06-25 21:43:37 +0900

    feat: 뒤로가기 버튼

--- Eiiii/src/pages/Notice.jsx
+++ Eiiii/src/pages/Notice.jsx
@@ -1,11 +1,14 @@
 import * as N from "../styles/pages/styledNotice"
 import Header from "../components/Header"
+import { useNavigate } from "react-router-dom"
 
 const data = Array(7).fill({
     content: "밥먹는 하마님이 점심 약속을 제안했어요. 확인해보세요."
 })
 
+
 const Notice = () => {
+    const navigate = useNavigate()
 
     return (
         <N.Container>
@@ -13,13 +16,14 @@ const Notice = () => {
                 leftIcon="back"
                 titleText="알림"
                 titleImg="meal"
+                onClickLeft={() => navigate(-1)}
             ></Header>
             <N.Text>새소식</N.Text>
             <N.NoticeWrapper>
             {data.map((data, idx) => (
                 <N.Item>
                 <N.Icon>
-                    <img
+                    <img 
                         src={`${process.env.PUBLIC_URL}/images/ring.svg`}
                         alt="ring"
                     />
@@ -29,6 +33,7 @@ const Notice = () => {
                 </N.Content>
                 </N.Item>
             ))}
+                
             </N.NoticeWrapper>
         </N.Container>
     )
