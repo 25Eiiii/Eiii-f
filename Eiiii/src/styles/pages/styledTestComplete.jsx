49b1d8e37f537ef24454ec75011932e16f4411b8
chaeyounggggggg <olivia0827@naver.com>
2025-06-26 20:53:47 +0900

    feat: 테스트 완

---
+++ Eiiii/src/styles/pages/styledTestComplete.jsx
@@ -0,0 +1,45 @@
+import {styled} from "styled-components";
+
+export const Container = styled.div`
+position: relative;
+  margin: 0 auto;
+  width: 440px;
+  height: 956px;
+  background: #FFFFFF;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  border-radius: 30px;
+  border: 1px solid #6A5656;
+  margin-top:10vh;
+`;
+
+export const Logo = styled.div`
+margin-top:214px;
+`;
+
+export const Text1 = styled.div`
+width: 382px;
+flex-shrink: 0;
+color: #606060;
+margin-top:69px;
+margin-bottom: 30px;
+text-align: center;
+font-family: "Spoqa Han Sans Neo";
+font-size: 20px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`;
+export const Text2 = styled.div`
+width: 382px;
+flex-shrink: 0;
+color: #606060;
+font-family: "Spoqa Han Sans Neo";
+font-size: 20px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+text-align: center;
+`;
+
